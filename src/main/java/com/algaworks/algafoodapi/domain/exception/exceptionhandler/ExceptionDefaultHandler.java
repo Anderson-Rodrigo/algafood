1d2e076a4be4e6b71f7d6525671df26b02837973
Anderson <anderson@microsys.inf.br>
2022-04-08 17:35:20 -0300

    fim do modulo 8

--- src/main/java/com/algaworks/algafoodapi/domain/exception/exceptionhandler/ExceptionDefaultHandler.java
+++ src/main/java/com/algaworks/algafoodapi/domain/exception/exceptionhandler/ExceptionDefaultHandler.java
@@ -1,7 +1,11 @@
 package com.algaworks.algafoodapi.domain.exception.exceptionhandler;
 
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
+import org.hibernate.PropertyValueException;
+import org.springframework.core.NestedExceptionUtils;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,14 +14,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 @Order(1)
 @RestControllerAdvice
 public class ExceptionDefaultHandler {
 
     @ResponseBody
-    @ExceptionHandler(value = {Exception.class, RuntimeException.class})
-    public ExceptionHandlerDto handleDefaultException(HttpServletRequest request, Exception ex) {
+    @ExceptionHandler(value = {Exception.class, RuntimeException.class, HttpMessageNotReadableException.class, PropertyBindingException.class})
+    public ExceptionHandlerDto handleDefaultException(HttpServletRequest request, Exception ex, RuntimeException rn) throws Exception {
+        if (ex.getClass().getAnnotation(ResponseStatus.class) != null) {
+            throw ex;
+        }
+
         ExceptionHandlerDto responseBody = new ExceptionHandlerDto();
 
         responseBody.setHandled(false);
@@ -27,11 +36,13 @@ public class ExceptionDefaultHandler {
         responseBody.setTitle("Exceção em tempo de execução do sistema");
         responseBody.setStatusCode(HttpStatus.BAD_REQUEST.value());
 
-        if (ex instanceof MethodArgumentNotValidException && ((MethodArgumentNotValidException) ex).getBindingResult() != null) {
+        Throwable properties = NestedExceptionUtils.getRootCause(ex);
+
+        if (ex instanceof MethodArgumentNotValidException) {
             if (((MethodArgumentNotValidException) ex).getBindingResult().getFieldErrorCount() > 0) {
                 for (FieldError error : ((MethodArgumentNotValidException) ex).getBindingResult().getFieldErrors()) {
                     responseBody.setTitle("Erro de validação de dados");
-                    responseBody.getDetails().add("O valor do campo '".concat(error.getField()).concat("' ").concat(error.getDefaultMessage()));
+                    responseBody.getDetails().add("O valor do campo '".concat(error.getField()).concat("' ").concat(Objects.requireNonNull(error.getDefaultMessage())));
                     responseBody.setHandled(true);
                 }
             }
@@ -42,6 +53,10 @@ public class ExceptionDefaultHandler {
         } else if (ex.getMessage().contains("Failed to convert value of type 'java.lang.String' to required type 'java.util.UUID'")) {
             responseBody.setTitle("Erro na nos parâmetros o sistema");
             responseBody.getDetails().add("Não foi possível interpretar o UUID.");
+            responseBody.setHandled(true);
+        } else if (ex.getCause().getMessage().contains("not-null property references a null or transient value")){
+            responseBody.setTitle("O campo não pode ser nulo.");
+            responseBody.getDetails().add(String.format("Não foi informado o campo %s", ((PropertyValueException) properties).getPropertyName()));
             responseBody.setHandled(true);
         }
 
